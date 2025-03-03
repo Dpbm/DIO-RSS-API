b6d7e80063eb5dc96d40fdbb78188498ffde5b50
Dpbm <dpbm136@gmail.com>
2025-03-03 18:53:01 -0300

    started project

---
+++ src/main/java/github/com/dpbm/rss/DioRssProjectApplication.java
@@ -0,0 +1,13 @@
+package github.com.dpbm.rss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioRssProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioRssProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github/com/dpbm/rss/DioRssProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package github.com.dpbm.rss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioRssProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
